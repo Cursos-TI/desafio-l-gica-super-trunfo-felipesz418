e7911f71fd4086a165fe01c9d2ad306e56411eed
felipesz418 <stuxsouza@gmail.com>
2025-09-08 19:28:37 +0000

    novato

---
+++ desafionovato.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    //Variaveis para cadastro primeira carta
+   char estado[20] = 'Estado';
+   char codigoCarta[03] = 'Cod';
+   char nomeDaCidade[30] = 'Cidade';
+   int populacao;
+   float area;
+   float pib;
+   int numeroDePontosTuristicos;
+
+    
+   //Variavies para cadastro segunda carta
+   char estado2[20] = 'Estado2';
+   char codigoCarta2[03] = 'Cod2';
+   char nomeDaCidade2[30] = 'Cidade2';
+   int populacao2;
+   float area2;
+   float pib2;
+   int numeroDePontosTuristicos2;
+
+
+   printf("Insira o estado ")
+
+}
